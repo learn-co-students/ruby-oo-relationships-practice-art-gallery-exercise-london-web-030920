0d4508db61079ef9734d25738fea6d23e977d6ea
julianwakefield <julianwakefield@outlook.com>
2020-03-16 15:19:46 +0000

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,17 @@
 class Gallery
 
   attr_reader :name, :city
+  
+  @@all = []
 
-  def initialize(name, city)
+  def initialize(name, city, artist, painting)
     @name = name
     @city = city
+    @artist = artist
+    @painting = painting
+
+    @@all << self
+
   end
 
 end
