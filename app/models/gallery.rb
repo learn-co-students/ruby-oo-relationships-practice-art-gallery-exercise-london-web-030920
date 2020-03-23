6681643bbc10bab274d0794ee2395b72a4d5819e
thealekid <chukorakwusi@gmail.com>
2020-03-23 09:11:37 +0000

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,30 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    
+    @@all << self
+
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.map {|painting| painting.artist}.uniq
+  end
+
+  def artist_names
+    artist.map {|artist| artist.name}
+  end
+
+  def self.all
+    @@all
   end
 
 end
